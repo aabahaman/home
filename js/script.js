cab3665ddc894d4bce34ebd4331b2db111b7f4ec
aabahaman <72589224+aabahaman@users.noreply.github.com>
2024-09-24 01:37:31 +0530

    added sheet doGet

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var AppsScriptLink = "AKfycbwClrgQiW-LnbEZ5l7u6ZHAA3PPT-5Ix1h4ZlocnRvnGAqEeKGeUYqegsgzQ-pqZRwS";
+
+function userVal() {
+    const username = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    return new Promise((resolve, reject) => {
+      $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=userProfile&user=" + username + "&pw=" + password,
+        function (data) {
+          console.log(data);
+          resolve(data);
+        }).fail(function(jqxhr, textStatus, error) {
+          reject(error);
+        });
+    });
+  }
+
+  
