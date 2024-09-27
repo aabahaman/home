0552a6c57b76ec879fd5392b0e701c07411f890e
aabahaman <72589224+aabahaman@users.noreply.github.com>
2024-09-27 12:00:47 +0530

    adding downloads

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 var AppsScriptLink = "AKfycbwClrgQiW-LnbEZ5l7u6ZHAA3PPT-5Ix1h4ZlocnRvnGAqEeKGeUYqegsgzQ-pqZRwS";
 
 function userVal() {
-    const username = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
-    return new Promise((resolve, reject) => {
-      $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=userProfile&user=" + username + "&pw=" + password,
-        function (data) {
-          // console.log(data);
-          resolve(data);
-        }).fail(function(jqxhr, textStatus, error) {
-          reject(error);
-        });
-    });
-  }
+  const username = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  return new Promise((resolve, reject) => {
+    $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=userProfile&user=" + username + "&pw=" + password,
+      function (data) {
+        // console.log(data);
+        resolve(data);
+      }).fail(function(jqxhr, textStatus, error) {
+        reject(error);
+      });
+  });
+}
 
 function billingData() {
-    return new Promise((resolve, reject) => {
-      $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=billingData",
-        function (data) {
-          // console.log(data);
-          resolve(data);
-        }).fail(function(jqxhr, textStatus, error) {
-          reject(error);
-        });
-    });
-  }
+  return new Promise((resolve, reject) => {
+    $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=billingData",
+      function (data) {
+        // console.log(data);
+        resolve(data);
+      }).fail(function(jqxhr, textStatus, error) {
+        reject(error);
+      });
+  });
+}
 
-  
+function downloadsData() {
+  return new Promise((resolve, reject) => {
+    $.getJSON("https://script.google.com/macros/s/" + AppsScriptLink + "/exec?page=downloads",
+      function (data) {
+        // console.log(data);
+        resolve(data);
+      }).fail(function(jqxhr, textStatus, error) {
+        reject(error);
+      });
+  });
+}
+
